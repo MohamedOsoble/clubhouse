4704286e66989bdbe49c1a31a5886fe3724ddcd8
Mohamed Osoble <mohamedosoble@outlook.com>
2025-10-05 17:53:04 +0100

    fix db url

--- db/pool.js
+++ db/pool.js
@@ -25,7 +25,6 @@ if (process.env.NODE_ENV === "PROD") {
 }
 
 module.exports.pool = new Pool({
-  connectionString:
-    "postgresql://clubhouse_vulz_user:mCHSKoic1PEEnt40Hxrw8psAYr7JVxwv@dpg-d3h9pee3jp1c73fchol0-a/clubhouse_vulz",
+  connectionString: process.env.internal_db_url,
 });
 module.exports.dbConfig = config;
